9b5ce25275361e484cca5ed640f04744ebf54024
David Gudeman <davidgudeman@Davids-MacBook-Pro.local>
2014-08-13 23:21:51 -0700

    fourth

--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,8 @@
-$(function(){
+$(function(){//this makes sure the javascript is run after the page loads
 
 
 
-	$('.slide-viewer').each(function(){
+	$('.slide-viewer').each(function(){ //calls this on every image slide-viewer but we only have one
 		var $this = $(this);
 		var $group = $this.find('.slide-group');
 		var $slides = $this.find('.slide');
@@ -12,17 +12,17 @@ $(function(){
 
 
 		function changeSlide(buttonIndex) {
-			buttonArray[currentIndex].removeClass('active');
-			currentIndex = buttonIndex;
+			buttonArray[currentIndex].removeClass('active');//removes active from button
+			currentIndex = buttonIndex;//reassigns the current index
 			$slides.each(function(slideIndex) {
 				if (buttonIndex === slideIndex) {
-					$(this).show();
+					$(this).show(); //'this' is refering to the particular slide we are on as we cycle through all the slides
 					
 				} else {
 					$(this).hide();
 				}
 			});
-			buttonArray[currentIndex].addClass('active');
+			buttonArray[currentIndex].addClass('active');//adds active to the correct button
 		}
 
 
@@ -30,14 +30,14 @@ $(function(){
 			//Create a button element for the button
 			var $button = $('<button type="button" class="slide-btn">&bull;</button>');
 			if (index === currentIndex) {
-				$button.addClass('active');
+				$button.addClass('active');//Makes the first button active
 			}
 			$button.on('click', function() {
 				changeSlide(index);
 			});
 
-			$button.appendTo('.slide-buttons');
-			buttonArray.push($button)
+			$button.appendTo('.slide-buttons');//puts the buttons in the correct div
+			buttonArray.push($button)//adds the current button to the array
 		});
 	});
 
